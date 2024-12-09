f73d9b61a1c41c9bd475a2742dee742cd5daed12
Elijah Melton <elimelt@uw.edu>
2024-12-08 23:10:38 -0800

    fun

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -592,6 +592,9 @@ const HomePage: React.FC = () => {
 
   return (
     <div className="fixed inset-0 w-screen h-screen overflow-hidden">
+      <a href="/" className="absolute top-4 left-4 z-40 text-white">
+
+      </a>
       <canvas
         ref={canvasRef}
         width={dimensions.width}
